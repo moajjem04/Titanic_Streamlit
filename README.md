e10deb247c1832b84e93ac4c0287d29da94d9127
moajjem04 <moajjem04@gmail.com>
2021-04-23 05:16:53 +0600

    Added set_page_config and modfied readme

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Streamlit App for Titanic Dataset
-[![Heroku](https://pyheroku-badge.herokuapp.com/?app=survive-titanic&style=flat)](https://survive-titanic.herokuapp.com)
+![Heroku](https://pyheroku-badge.herokuapp.com/?app=survive-titanic&style=flat)
+
+Here is the [app](https://survive-titanic.herokuapp.com) deployed in Heroku!
 
 [//]: # (heroku link - https://survive-titanic.herokuapp.com)
--- titanic.py
+++ titanic.py
@@ -1,17 +1,24 @@
 # D:
-# Python Projects\Titanic_Streamlit
+# path : Python Projects\Titanic_Streamlit
 # env : titanic
 import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
 from PIL import Image
+# https://emojipedia.org for emoji links
+st.set_page_config(
+    page_title="Survive Titanic!",
+    page_icon=":ocean:",
+    layout="centered",
+    initial_sidebar_state="auto")
 
 # Title
 st.title("Would you Survive Titanic?")
 # Introduction
 with open('introduction.txt') as f:
   intro = f.readlines()
+st.write("")
 st.write(intro[0])
 
 image = Image.open('Image/jack and rose.jfif')
@@ -19,18 +26,19 @@ st.image(image, caption='Jack and Rose')
 
 # Configuring Sidebar
 st.sidebar.header('Please enter your information!')
+Pclass_dict = {'1st Class': 1,'2nd Class': 2,'3rd Class': 3} # convert strings to class numbers
 
 def user_input_features():
-    sex = st.sidebar.selectbox('Gender',('male','female'))
-    Pclass = st.sidebar.selectbox('Ticket Class',(1,2,3))
-    Age = st.sidebar.number_input('Age(years)', 0,100,28)
-    alone = st.sidebar.selectbox('Travelling Alone?',('Yes','No'))
-    data = {'Pclass': Pclass,
-            'Sex': sex,                
-            'Age': Age,
-            'Is_alone': alone}
-    features = pd.DataFrame(data, index=[0])
-    return features
+  sex = st.sidebar.selectbox('Gender',('male','female'))
+  Pclass = st.sidebar.selectbox('Ticket Class',('1st Class','2nd Class','3rd Class'))
+  Age = st.sidebar.number_input('Age(years)', 0,100,28)
+  alone = st.sidebar.selectbox('Travelling Alone?',('Yes','No'))
+  data = {'Pclass': Pclass_dict[Pclass],
+          'Sex': sex,                
+          'Age': Age,
+          'Is_alone': alone}
+  features = pd.DataFrame(data, index=[0])
+  return features
 
 # Getting Input
 input_df = user_input_features()
@@ -54,6 +62,7 @@ result = result[:,1]
 st.sidebar.header('Prediction')
 st.sidebar.write(f"*You have a **{result[0]*100:.2f}%** chance of surviving Titanic!*")
 # note < add flavour texts in the sidebar, looks better.
+# st.beta_expander <- use this for extra details
 # st.sidebar.write(f"You have a {result[0]*100:.2f}% chance of surviving Titanic!")
 # st.sidebar.write(f"You have a {result[0]*100:.2f}% chance of surviving Titanic!")
 # st.sidebar.write(f"You have a {result[0]*100:.2f}% chance of surviving Titanic!")
@@ -62,7 +71,7 @@ st.sidebar.write(f"*You have a **{result[0]*100:.2f}%** chance of surviving Tita
 
 
 # History Flavor Text
-st.subheader('History of Titanic')
+st.header('History of Titanic')
 with open('description.txt') as f:
   description = f.readlines()
 st.write(description[0])
